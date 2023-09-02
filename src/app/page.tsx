8c526a06b5e258691da102802ee1fd7b202cffdd
MezoAli <moutaz.ali.dev@gmail.com>
2023-09-02 19:11:49 +0300

    adding again all server side fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,25 @@ import { Room } from "@/types/roomTypes";
 import PaddingContainer from "../components/PaddingContainer";
 import RoomCard from "../components/RoomCard";
 
-// const getAllRooms = async () => {
-//   try {
-//     const response = await fetch(`${process.env.SITE_URL}/api/rooms`);
-//     const data = await response.json();
-//     return data;
-//   } catch (error) {
-//     return { error };
-//   }
-// };
+const getAllRooms = async () => {
+  try {
+    const response = await fetch(`${process.env.SITE_URL}/api/rooms`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return { error };
+  }
+};
 
 export default async function Home() {
-  // const { rooms } = await getAllRooms();
+  const { rooms } = await getAllRooms();
 
   return (
     <PaddingContainer>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-        {/* {rooms?.map((room: any) => {
+        {rooms?.map((room: any) => {
           return <RoomCard key={room._id} room={room} />;
-        })} */}
+        })}
       </div>
     </PaddingContainer>
   );
--- src/app/rooms/[roomId]/page.tsx
+++ src/app/rooms/[roomId]/page.tsx
@@ -6,36 +6,36 @@ interface RoomPageProps {
   };
 }
 
-// const getRoomData = async (roomId: string) => {
-//   try {
-//     const response = await fetch(`${process.env.SITE_URL}/api/rooms/${roomId}`);
-//     const data = await response.json();
-//     return data;
-//   } catch (error) {
-//     return { error };
-//   }
-// };
+const getRoomData = async (roomId: string) => {
+  try {
+    const response = await fetch(`${process.env.SITE_URL}/api/rooms/${roomId}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return { error };
+  }
+};
 
-// export async function generateStaticParams() {
-//   const response = await fetch("http://localhost:3000/api/rooms");
-//   const data = await response.json();
-//   return data?.rooms?.map((room: Room) => ({
-//     roomId: room?._id,
-//   }));
-// }
+export async function generateStaticParams() {
+  const response = await fetch("http://localhost:3000/api/rooms");
+  const data = await response.json();
+  return data?.rooms?.map((room: Room) => ({
+    roomId: room?._id,
+  }));
+}
 
-// export async function generateMetadata({ params: { roomId } }: RoomPageProps) {
-//   const roomData = await getRoomData(roomId);
-//   const room: Room = roomData.room;
-//   return {
-//     title: room?.name,
-//     description: room?.description,
-//   };
-// }
+export async function generateMetadata({ params: { roomId } }: RoomPageProps) {
+  const roomData = await getRoomData(roomId);
+  const room: Room = roomData.room;
+  return {
+    title: room?.name,
+    description: room?.description,
+  };
+}
 
 const RoomPage = async ({ params: { roomId } }: RoomPageProps) => {
-  // const room = await getRoomData(roomId);
-  // console.log(room);
+  const room = await getRoomData(roomId);
+  console.log(room);
 
   return <div>room Id {roomId}</div>;
 };
